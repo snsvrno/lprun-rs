e3de5dc227a8f32fd408e788b39e3c86e340353e
snsvrno <snsvrno@tuta.io>
2018-02-23 04:35:14 -0700

    adds debug output

--- src/bin/lprun.rs
+++ src/bin/lprun.rs
@@ -5,11 +5,15 @@ extern crate lprun;
 extern crate ansi_term;
 
 use lprun::interface;
+use std::env;
 
 fn main() {  
   // builds the app
   let app = interface::app()
+    .arg(clap::Arg::with_name("debug").long("debug").help("Shows additional information about commands run."))
     .get_matches();
+
+  if app.is_present("debug") { env::set_var("OUTPUT_DEBUG_ENABLED","true"); }
 
   // processess the arguement matches.
   match interface::process(&app) {
